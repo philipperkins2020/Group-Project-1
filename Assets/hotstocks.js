53f215c5cdcf8ba3c295f9fb12d9043ed4e0996c
Morgan Coborn <mtcoborn@gmail.com>
2021-03-17 21:36:36 -0500

    changed button colors, stock watch list local storage

--- Assets/hotstocks.js
+++ Assets/hotstocks.js
@@ -1,1 +1,13 @@
-console.log ("helloworld")
+// Save var
+function saveStock(event) {
+    event.preventDefault()
+    var topStock = document.getElementById("stockInput").value;
+    console.log(topStock)
+    localStorage.setItem("topStock", topStock);
+}
+// Save Value
+function stockValue() {
+    return localStorage.getItem("topStock");
+}
+console.log(stockValue());
+document.getElementById("stockButton").addEventListener("click", saveStock)
