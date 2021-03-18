5fa84938f403c0cee99794c39b73bebc68555d78
PhilPerk <philipperkins2020@gmail.com>
2021-03-17 21:38:12 -0500

    commit

--- Assets/hotstocks.js
+++ Assets/hotstocks.js
@@ -1,1 +1,48 @@
-console.log ("helloworld")
+
+
+// Save var
+
+
+
+
+
+
+// Save var
+function saveStock(event) {
+    
+    event.preventDefault()
+    
+    var topStock = document.getElementById('stockInput').value;
+    console.log(topStock)
+    
+    var topCrypto = document.getElementById('cryptoInput').value;
+    console.log(topCrypto)
+
+    
+    localStorage.setItem('topStock', topStock);
+    localStorage.setItem('topCrypto', topCrypto);
+
+
+}
+
+// Save Value
+function stockValue() {
+
+
+
+    return localStorage.getItem('topstock');
+}
+console.log(stockValue());
+
+
+document.getElementById('stockButton').addEventListener("click", saveStock)
+document.getElementById('cryptoButton').addEventListener("click", saveStock)
+
+
+
+
+
+
+
+
+
